92dd1e0fcebc53f2f479f387e2eda207eec8ca0b
obyron88 <CODA2017@yahoo.com>
2017-07-20 13:44:09 +0200

    sec

--- form.php
+++ form.php
@@ -4,27 +4,27 @@ class Form{
   public function startform(){
     return ('<form action="#" method="post">');
   }
-  public function input($placeholder){
-    return ('<input placeholder="'.$placeholder.'" type="text" name="" value=""><br>');
+  public function input(){
+    return ('<input placeholder="e-mail" type="text" name="email" value=""><br>');
   }
   public function select($option1, $option2, $option3){
-    return ('<select value="">
-    <option value="">'.$option1.'</option>
-    <option value="">'.$option2.'</option>
-    <option value="">'.$option3.'</option>
+    return ('<select name="sel" value="">
+    <option  value="'.$option1.''.$option1.'">'.$option1.'</option>
+    <option  value="'.$option2.'">'.$option2.'</option>
+    <option  value="'.$option3.'">'.$option3.'</option>
     </select><br>');
   }
   public function submit($button){
     return ('<button type="submit">'.$button.'</button><br>');
   }
-  public function textarea($textarea){
-    return ('<textarea placeholder="'.$textarea.'"value=""></textarea><br>');
+  public function textarea(){
+    return ('<textarea name="sauce" placeholder="Quelle sauce ?"value=""></textarea><br>');
   }
   public function radio(){
-    return ('<input type="radio" value=""><br>');
+    return ('<input name="rad" type="radio" value=""><br>');
   }
   public function checkbox(){
-    return ('<input type="checkbox" value="">');
+    return ('<input name="check" type="checkbox" value="">');
   }
   public function endform(){
     return ('</form>');
@@ -49,7 +49,7 @@ class Html{
     return ('<script src="script.js"></script>');
   }
   public function image(){
-    return ('<img src="slow-classic-PP-e1430487826466.jpg">');
+    return ('<img src="slow-classic-PP-e1430487826466.jpg"><br>');
   }
   public function a($lien, $nomlien){
     return ('<a href="'.$lien.'">'.$nomlien.'</a>');
@@ -57,6 +57,45 @@ class Html{
 }
 
 class Validator {
+  // Vérifie si une valeur est égale à une autre
+  // Passe les 2 valeurs directement en paramètre.
+  public static function isEqual($value1, $value2)
+  {
+      return $value1 === $value2;
+  }
 
+  //Vérifie si une valeur est égale à zéro
+  public static function notZero($value)
+  {
+      return $value !== 0;
+  }
+
+  // Vérifie que le mail est valide
+  public static function isValidMail($uu)
+  {
+      return filter_var($uu, FILTER_VALIDATE_EMAIL);
+  }
+
+  // Vérifie que ce soit bien un entier
+  public static function isInt($int)
+  {
+      return filter_var($int, FILTER_VALIDATE_INT);
+  }
+
+  // Vérifie que ce soit bien un float (ou int)
+  public static function isFloat($float)
+  {
+      return filter_var($float, FILTER_VALIDATE_FLOAT);
+  }
+
+  // Vérifie que l'image envoyé est bien un "jpg"
+  // public static function isJpg($index)
+  // {
+  //     if(is_uploaded_file(($_FILES[$index]['tmp_name'])) {
+  //         $path_parts = pathinfo($_FILES[$index]['name']);
+  //         return $path_parts['extension'] === 'jpg';
+  //     }
+  //     else throw new Exception('Mec, c\'est pas une image ça !');
+  // }
 
 }
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 require 'form.php';
 
+$whatsauce = $_POST['sauce'];
+$email = $_POST['email'];
+
 $form = new Form($_POST);
 
 echo $form->startform();
-echo $form->input('Quelle cuisson?');
+echo $form->input();
 echo $form->select('Porc', 'Boeuf', 'Poulet');
 echo $form->submit("J'ai faim !");
-echo $form->textarea('Quelle sauce?');
+echo $form->textarea();
 echo $form->radio();
 echo $form->checkbox();
 echo $form->endform();
@@ -21,3 +24,7 @@ echo $link->jqueryui();
 echo $link->scriptjs();
 echo $link->image();
 echo $link->a('http://a141.idata.over-blog.com/4/91/81/31/gateau-d-anniversaire/Foret-noire-3.jpg', 'Dessert');
+
+$val = new Validator();
+
+echo $val->isValidMail($email);
